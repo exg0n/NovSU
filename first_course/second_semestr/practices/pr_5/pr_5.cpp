17f92bc4f8976d6e39947444b11dc302f84350ed
exgon <105127448+exg0n@users.noreply.github.com>
2024-02-26 11:29:56 +0300

    Add files via upload

---
+++ first_course/second_semestr/practices/pr_5/pr_5.cpp
@@ -0,0 +1,18 @@
+﻿// Вариант 9
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    ifstream file_input("qwe.txt");
+    ifstream file_output("qwe.bin", ios::binary);
+
+    if (!file_input.is_open() || !file_output.is_open())
+        cerr << "Не удалось открыть файлю Попробуйте ещё раз.";
+
+    while (getline(file_input, ))
+}
