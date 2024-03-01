8e07f80a9f1961ce30dfda8106549c4f3b0b6d28
exgon <105127448+exg0n@users.noreply.github.com>
2024-03-01 12:42:38 +0300

    Add files via upload

--- first_course/second_semestr/practices/pr_5/pr_5.cpp
+++ first_course/second_semestr/practices/pr_5/pr_5.cpp
@@ -8,11 +8,38 @@ using namespace std;
 
 int main()
 {
-    ifstream file_input("qwe.txt");
-    ifstream file_output("qwe.bin", ios::binary);
+    setlocale(LC_ALL, "Russian");
 
-    if (!file_input.is_open() || !file_output.is_open())
-        cerr << "Не удалось открыть файлю Попробуйте ещё раз.";
+    ifstream input_file("qwe.txt");
+    if (!input_file)
+        cerr << "Не удалось открыть текстовый файл";
 
-    while (getline(file_input, ))
+    ofstream output_file("qwe.bin", ios::binary);
+    if (!output_file)
+        cerr << "Не удалось открыть бинарный файл";
+
+    string line;
+    while (getline(input_file, line)) {
+        // Записываем предложение в бинарный файл
+        output_file.write(line.c_str(), line.size());
+        // Добавляем разделитель между предложениями
+        char delimiter = '.';
+        output_file.write(&delimiter, sizeof(char));
+    }
+
+    input_file.close();
+    output_file.close();
+
+    // Открываем бинарный файл для чтения и выводим его содержимое на консоль
+    ifstream binary_file("qwe.bin", ios::binary);
+    if (!binary_file)
+        cerr << "Не удалось открыть бинарный файл";
+
+    cout << "Содержимое бинарного файла: " << endl;
+    char character;
+    while (binary_file.get(character))
+        cout << character;
+
+    // Закрываем бинарный файл
+    binary_file.close();
 }
